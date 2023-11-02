71a06c0bb1efd6b47238ccf007903b5188fbe46a
Pralhad Kamath <pralhad.kamath@pratishthanventures.com>
2023-11-02 17:16:25 +0530

    Added request & response to online app.

--- scripts/prod-setup.sh
+++ scripts/prod-setup.sh
@@ -6,4 +6,8 @@ mkdir tv-data
 mkdir tv-data/low-tf-data
 mkdir tv-data/base-data
 read -p "Please Enter Dropbox Path: E.g. /Users/user/Dropbox:" -r dropbox
-ln -sf "$dropbox"/Trader .
+ln -sf "$dropbox"/Trader/secret .
+ln -sf "$dropbox"/Trader/traderv3/logs/archive logs/archive
+
+
+
