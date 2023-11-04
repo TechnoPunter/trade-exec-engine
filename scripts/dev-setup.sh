94737db917d4e3353967f6fc3089e784fcaf6fb8
Pralhad Kamath <pralhad.kamath@pratishthanventures.com>
2023-11-04 21:31:06 +0530

    Fixing dev-setup.sh

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -1,5 +1,23 @@
 #!/bin/zsh
 
+MODULE_NAME=trade-exec-engine
+CURR_PATH=$(pwd)
+
+if [[ $CURR_PATH == *$MODULE_NAME* ]]; then
+  sleep 1
+else
+  echo "Need to run from trade-exec-engine location"
+  exit 1
+fi
+
+if [[ $CURR_PATH == *scripts* ]]; then
+  CURR_PATH="${CURR_PATH%%scripts*}"
+fi
+
+cd "$CURR_PATH" || exit 1
+
+STRATS_PATH="$CURR_PATH"/../prediction-models/strategies
+ln -sf "$STRATS_PATH" predict/strategies
 
 mkdir logs
 mkdir tv-data
@@ -7,3 +25,5 @@ mkdir tv-data/low-tf-data
 mkdir tv-data/base-data
 read -p "Please Enter Dropbox Path: E.g. /Users/user/Dropbox:" -r dropbox
 ln -sf "$dropbox"/Trader .
+
+echo "Done!"
