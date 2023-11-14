60d1e12f5bd48a6a006167f3f501358a4215da78
Pralhad Kamath <pralhad.kamath@pratishthanventures.com>
2023-11-14 21:24:09 +0530

    Add params to engine script.

--- scripts/exec-engine.sh
+++ scripts/exec-engine.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-cd /var/www/trade-exec-engine
+BASE_DIR=/var/www/trade-exec-engine
+cd "$BASE_DIR"
 
 if [ -z "$1" ]; then
   echo "Error: missing Account parameter."
@@ -12,6 +13,9 @@ today="$(date -I)"
 now="$(date +"%Y-%m-%d_%H-%M-%S")"
 export ACCOUNT=${1}
 echo "Account:${ACCOUNT}"
+export GENERATED_PATH="$BASE_DIR"/generated
+export RESOURCE_PATH="$BASE_DIR"/resources/config
+export LOG_PATH="$BASE_DIR"/logs
 sleep 1
 python run-engine.py "${ACCOUNT}" 1> logs/exec-engine-"${ACCOUNT}".log 2> logs/exec-engine-"${ACCOUNT}".err
 mkdir logs/archive/${today} 2> /dev/null
