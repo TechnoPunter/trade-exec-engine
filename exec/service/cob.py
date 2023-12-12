3696250d1fbff52dc3dbc8470986ce72b81b5a34
Pralhad Kamath <pralhad.kamath@pratishthanventures.com>
2023-12-12 15:10:12 +0530

    Adding MTM for COB.

--- exec/service/cob.py
+++ exec/service/cob.py
@@ -46,6 +46,7 @@ class CloseOfBusiness:
         self.cob_date = None
 
     def setup(self, acct: str, cob_date: str, params: pd.DataFrame = None):
+        logger.debug(f"Entered cob setup for {acct} & cob: {cob_date}")
         self.acct = acct
         self.cob_date = cob_date
         self.shoonya = Shoonya(self.acct)
@@ -53,12 +54,14 @@ class CloseOfBusiness:
             self.params = load_params(api=self.shoonya, acct=acct)
         else:
             self.params = params
+        logger.debug(f"Params:\n{params}")
         self.params.loc[self.params.entry_order_id.isna(), 'entry_order_status'] = 'INVALID'
         self.params.loc[:, 'active'] = 'N'
         self.sds = ScripDataService(shoonya=self.shoonya, trader_db=self.trader_db)
 
     def store_broker_trades(self, acct: str = None, cob_date: str = None, shoonya: Shoonya = None,
                             ls: LogService = None, params: pd.DataFrame = None):
+        logger.debug(f"Starting store broker trades for {acct} & cob {cob_date}")
         if acct is None:
             acct = self.acct
         if cob_date is None:
@@ -71,7 +74,9 @@ class CloseOfBusiness:
             predicate = f"m.{PARAMS_HIST}.acct == '{acct}'"
             predicate += f",m.{PARAMS_HIST}.trade_date == '{cob_date}'"
             params = self.trader_db.query_df(PARAMS_HIST, predicate=predicate)
+        logger.debug(f"Params:\n{params}")
         orders = shoonya.api_get_order_book()
+        logger.debug(f"Orders:\n{orders}")
         if orders is None:
             logger.error(f"__store_broker_trades: No Broker orders to store")
             return
@@ -80,7 +85,7 @@ class CloseOfBusiness:
             return
         else:
             ls.log_entry(log_type=BROKER_TRADE_LOG_TYPE, keys=["COB"], data=orders, log_date=cob_date, acct=acct)
-            logger.info(f"__store_broker_trades: Broker Trades created for {acct}")
+            logger.info(f"store_broker_trades: Broker Trades created for {acct}")
 
             # Update entry, SL, Target Prices in Params from OB
             orders_df = pd.DataFrame(orders)
@@ -102,20 +107,24 @@ class CloseOfBusiness:
             params = pd.merge(params, orders_df, how="left", left_on="target_order_id", right_on="norenordno")
             params.drop(["norenordno"], axis=1, inplace=True)
             params.rename(columns={"avgprc": "target_price", "entry_ts": "entry_time"}, inplace=True)
+
             params.drop(["entry_order_id", "sl_order_id", "target_order_id"], axis=1, inplace=True)
 
             params[["status", "exit_time", "exit_price", "pnl"]] = params.apply(calc_order_stats, axis=1,
                                                                                 result_type='expand')
             params.fillna(0, inplace=True)
+
+            logger.debug(f"About to store trade_log:\n{params}")
             predicate = f"m.{TRADE_LOG}.acct == '{acct}'"
             predicate += f",m.{TRADE_LOG}.trade_date == '{cob_date}'"
             predicate += f",m.{TRADE_LOG}.trade_type == 'BROKER'"
             self.trader_db.delete_recs(TRADE_LOG, predicate=predicate)
             self.trader_db.bulk_insert(TRADE_LOG, data=params)
-            logger.info("Done")
+            logger.info("store_broker_trades: Done")
 
     def store_bt_trades(self, acct: str = None, cob_date: str = None, params: pd.DataFrame = None,
                         exec_mode: str = "SERVER", sds: ScripDataService = None, ls: LogService = None):
+        logger.debug(f"Starting store bt trades for {acct} & cob {cob_date}")
         if acct is None:
             acct = self.acct
         if cob_date is None:
@@ -129,12 +138,15 @@ class CloseOfBusiness:
         if len(params) == 0:
             logger.error(f"Unable to find params for {acct} for {cob_date}")
             return
+        logger.debug(f"Params:\n{params}")
         scrips = list(set(params.scrip))
 
         sds.load_scrips_data(scrip_names=scrips, opts=["TICK"])
+        logger.debug(f"Tick Data loaded for {scrips}")
 
         f = FastBT(exec_mode=exec_mode)
         bt_trades, _, bt_mtm = f.run_cob_accuracy(params=params)
+        logger.debug(f"run_cob_accuracy: Completed with BT trades: {len(bt_trades)} & {len(bt_mtm)} entries")
         if len(bt_trades) > 0:
             if ls is None:
                 ls = self.ls
@@ -147,6 +159,7 @@ class CloseOfBusiness:
                 'date': 'trade_date',
                 'strategy': 'model'}, inplace=True)
 
+            logger.debug(f"About to store trade_log:\n{bt_trades}")
             ls.log_entry(log_type=BT_TRADE_LOG_TYPE, keys=["COB"], data=bt_trades, log_date=cob_date, acct=acct)
             predicate = f"m.{TRADE_LOG}.acct == '{acct}'"
             predicate += f",m.{TRADE_LOG}.trade_date == '{cob_date}'"
@@ -154,12 +167,16 @@ class CloseOfBusiness:
             self.trader_db.delete_recs(TRADE_LOG, predicate=predicate)
             self.trader_db.bulk_insert(TRADE_LOG, data=bt_trades)
 
-            # bt_mtm_dict = {}
-            # for key, value in bt_mtm.items():
-            #     date_columns = value.select_dtypes(include=['datetime64']).columns.tolist()
-            #     value[date_columns] = value[date_columns].astype(str)
-            #     bt_mtm_dict[key] = value.to_dict(orient="records")
-            # ls.log_entry(log_type=BT_MTM_LOG_TYPE, keys=["COB"], data=bt_mtm_dict, log_date=cob_date, acct=acct)
+        if len(bt_mtm) > 0:
+            predicate = f"m.{TRADES_MTM_TABLE}.acct == '{acct}'"
+            predicate += f",m.{TRADES_MTM_TABLE}.trade_date == '{cob_date}'"
+            self.trader_db.delete_recs(TRADES_MTM_TABLE, predicate=predicate)
+            for key, bt_mtm_entries in bt_mtm.items():
+                bt_mtm_entries['time'] = bt_mtm_entries['time'].astype(int)
+                bt_mtm_entries.fillna(0, inplace=True)
+                bt_mtm_entries = bt_mtm_entries.assign(acct=acct)
+                logger.debug(f"About to store trade_log for {key}:\n{bt_mtm_entries}")
+                self.trader_db.bulk_insert(TRADES_MTM_TABLE, data=bt_mtm_entries)
         else:
             logger.error(f"No records in BT Trades for {self.acct}")
             self.ls.log_entry(log_type=BT_TRADE_LOG_TYPE, keys=["COB"], data=pd.DataFrame(), log_date=self.cob_date,
@@ -174,12 +191,15 @@ class CloseOfBusiness:
         :param params:
         :return:
         """
+        logger.debug(f"Entered store params for {acct} & cob: {cob_date}")
         if acct is None:
             acct = self.acct
         if cob_date is None:
             cob_date = self.cob_date
         if params is None:
             params = self.params
+
+        logger.debug(f"Params:\n{params}")
 
         if len(params) > 0:
             self.ls.log_entry(log_type=PARAMS_LOG_TYPE, keys=["COB"], data=params, log_date=cob_date, acct=acct)
@@ -188,6 +208,7 @@ class CloseOfBusiness:
             predicate = f"m.{PARAMS_HIST}.acct == '{acct}'"
             predicate += f",m.{PARAMS_HIST}.trade_date == '{cob_date}'"
             self.trader_db.delete_recs(PARAMS_HIST, predicate=predicate)
+            logger.debug(f"About to store:\n{db_params}")
             self.trader_db.bulk_insert(PARAMS_HIST, data=db_params)
             logger.info(f"store_params: Orders created for {acct}")
         else:
@@ -201,6 +222,7 @@ class CloseOfBusiness:
             2. self.store_broker_trades() - Store Trades to DB
             3. self.store_bt_trades() - Store Backtesting results to DB
         """
+        logger.info(f"Start COB for {accounts} for {cob_date} with opts: {opts}")
         if opts is None:
             opts = ["setup", "store_params", "store_broker_trades", "store_bt_trades"]
 
@@ -214,19 +236,28 @@ class CloseOfBusiness:
             else:
                 params = None
             if "setup" in opts:
+                logger.info(f"Starting setup for {account}")
                 self.setup(acct=account, cob_date=cob_date, params=params)
             if "store_params" in opts:
+                logger.info(f"Starting store_params for {account}")
                 self.store_params(acct=account, cob_date=cob_date, params=params)
             if "store_broker_trades" in opts:
+                logger.info(f"Starting store_broker_trades for {account}")
                 self.store_broker_trades(acct=account, cob_date=cob_date)
             if "store_bt_trades" in opts:
+                logger.info(f"Starting store_bt_trades for {account}")
                 self.store_bt_trades(acct=account, cob_date=cob_date, params=params)
+
+        logger.info("Completed COB Processing")
 
 
 if __name__ == '__main__':
     setup_logging("cob.log")
     c = CloseOfBusiness()
     accounts_ = 'Trader-V2-Pralhad'
-    # cob_ = '2023-12-08'
-    cob_ = None
-    c.run_cob(accounts=accounts_, cob_date=cob_, params_dict=None)
+    cob_ = '2023-12-08'
+    # cob_ = None
+    # c.run_cob(accounts=accounts_, cob_date=cob_, params_dict=None)
+    sh = Shoonya(acct=accounts_)
+    sds_ = ScripDataService(sh)
+    c.store_bt_trades(acct=accounts_, cob_date=cob_, exec_mode="LOCAL", sds=sds_)
